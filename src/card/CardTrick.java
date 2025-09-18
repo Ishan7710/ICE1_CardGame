0368aebf96b05cac1ec234872a3642daa6c237cf
ishan <ishan76579@gmail.com>
2025-09-18 10:09:39 -0400

    Added CardTrick.java with hard-coded lucky card

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -1,32 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package card;
+import java.util.Random;
 
-/**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
- */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+
+    private static final String[] SUITS = {"Hearts", "Diamonds", "Clubs", "Spades"};
+    private static final int HAND_SIZE = 7;
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        Card[] hand = new Card[HAND_SIZE];
+        for (int i = 0; i < HAND_SIZE; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(1 + rand.nextInt(13));
+            c.setSuit(SUITS[rand.nextInt(SUITS.length)]);
+            hand[i] = c;
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+
+        for (Card c : hand) {
+            System.out.println(c.getSuit() + " " + c.getValue());
+        }
+
+        Card luckyCard = new Card();
+        luckyCard.setValue(2);
+        luckyCard.setSuit("Spades");
+
+        boolean found = false;
+        for (Card c : hand) {
+            if (c.getValue() == luckyCard.getValue() && c.getSuit().equals(luckyCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Winner! The lucky card (" 
+                + luckyCard.getSuit() + " " + luckyCard.getValue() + ") is in the hand.");
+        } else {
+            System.out.println("Not this time. The lucky card (" 
+                + luckyCard.getSuit() + " " + luckyCard.getValue() + ") is NOT in the hand.");
+        }
     }
-    
 }
